584ee1f29cda99e7502553487f8cf3bb6980162a
pattuli <pratikbastakoti58@gmail.com>
2022-10-20 12:26:30 +0545

    Day-1 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String a = "Flutter";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Catalog')),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days of $a Learning"),
+        ),
+      ),
+      drawer: Drawer(
+        child: Text("Welcome to drawer."),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Flutter Learning"),
-          ),
-        ),
-      ),
+   return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
     );
   }
 }
